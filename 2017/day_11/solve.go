5cb2d3090fe3649ef53a68a84ead7acce2e2f42c
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2024-07-08 23:18:33 +0400

    add(2017): solve day 11 of 2017

---
+++ 2017/day_11/solve.go
@@ -0,0 +1,9 @@
+package day11
+
+func SolveOne(input any) any {
+    panic("Please implement SolveOne function")
+}
+
+func SolveTwo(input any) any {
+    panic("Please implement SolveTwo function")
+}
