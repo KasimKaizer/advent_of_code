611c5122fe4892e3421f46273798362cd768b4ba
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2024-07-23 11:47:32 +0400

    solve(2017): solve day 13 of 2017

---
+++ 2017/day_13/solve.go
@@ -0,0 +1,9 @@
+package day13
+
+func SolveOne(input any) (any, error) {
+    panic("Please implement SolveOne function")
+}
+
+func SolveTwo(input any) (any, error) {
+    panic("Please implement SolveTwo function")
+}
